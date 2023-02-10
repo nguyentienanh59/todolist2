3ec70d8efeed7cfbd468f8228b69d9f8c16b0f7e
Tien Anh <nguyentienanh5969@gmail.com>
2023-02-10 13:18:05 +0700

    update signup signin

--- public/js/main.js
+++ public/js/main.js
@@ -2,201 +2,208 @@ const todoListEl = document.querySelector(".todo-list");
 const todoOptionEls = document.querySelectorAll(".todo-option-item input");
 const todoInputEl = document.getElementById("todo-input");
 const btnAdd = document.getElementById("btn-add");
-const todosApi = "http://localhost:3000/todos";
+const todosApi = "https://api-training-xr6q.onrender.com/api/v1";
 let isUpdate = false;
 let idUpdate = null;
 let todos = [];
-let thisPage = 1;
-let limit = 6;
-let list = document.querySelectorAll(".todo-list .todo-item");
 
-function getTodoAPI(status) {
-  switch (status) {
-    case "all": {
-      return axios.get(`${todosApi}`);
+const instance = axios.create({
+  baseURL: "https://api-training-xr6q.onrender.com/api/v1/",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
-    case "active": {
-      return axios.get(`${todosApi}?status=true`);
-    }
-    case "unactive": {
-      return axios.get(`${todosApi}?status=false`);
-    }
-    default: {
-      return axios.get(`${todosApi}`);
-    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+instance.interceptors.response.use(
+  (response) => {
+    const accessToken = response.data.token;
+    localStorage.setItem("token", accessToken);
+
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-}
-function createTodoApi(title) {
-  return axios.post(`${todosApi}`, {
-    id: createId(),
-    title: title,
-    status: false,
-  });
-}
-function deleteTodoApi(id) {
-  return axios.delete(`${todosApi}/${id}`);
-}
+);
 
-function updateTodoApi(todo) {
-  return axios.put(`${todosApi}/${todo.id}`, {
-    title: todo.title,
-    status: todo.status,
-  });
-}
-
-function renderUI(arr) {
-  todoListEl.innerHTML = "";
-
-  // Kiểm tra mảng rỗng
-  if (arr.length == 0) {
-    todoListEl.innerHTML = "Không có công việc nào trong danh sách";
-    return;
+const getTodoAPI = async () => {
+  try {
+    const response = await instance.get(`${todosApi}/work`);
+    console.log(response);
+    // return response.data;
+  } catch (error) {
+    throw error;
   }
+};
+// console.log(getTodoList())
 
-  // Trường hợp có công việc
-  for (let i = 0; i < arr.length; i++) {
-    const t = arr[i];
-    todoListEl.innerHTML += `
-            <div class="todo-item ${t.status ? "active-todo" : ""}">
-                <div class="todo-item-title">
-                    <input 
-                        type="checkbox" 
-                        ${t.status ? "checked" : ""}
-                        onclick="toggleStatus(${t.id})"
-                    />
-                    <p>${t.title}</p>
-                </div>
-                <div class="option">
-                    <button class="btn btn-update" onclick="updateTitle(${
-                      t.id
-                    })">
-                        <img src="./img/pencil.svg" alt="icon" />
-                    </button>
-                    <button class="btn btn-delete" onclick=deleteTodo(${t.id})>
-                        <img src="./img/remove.svg" alt="icon" />
-                    </button>
-                </div>
-            </div>
-        `;
-  }
-}
-async function getTodos(status) {
-  const response = await getTodoAPI(status);
+// function createTodoApi(id, title) {
+//   return axios.post(`${todosApi}/work`, {
+//     id: id,
+//     title: title,
+//     status: false,
+//   });
+// }
+// function deleteTodoApi(id) {
+//   return axios.delete(`${todosApi}/work/${id}`);
+// }
+
+// function updateTodoApi(todo) {
+//   return axios.put(`${todosApi}/work/${todo.id}`, {
+//     title: todo.title,
+//     status: todo.status,
+//   });
+// }
+
+// function renderUI(arr) {
+//   todoListEl.innerHTML = "";
+
+//   // Kiểm tra mảng rỗng
+//   if (arr.length == 0) {
+//     todoListEl.innerHTML = "Không có công việc nào trong danh sách";
+//     return;
+//   }
+
+//   // Trường hợp có công việc
+//   for (let i = 0; i < arr.length; i++) {
+//     const t = arr[i];
+//     todoListEl.innerHTML += `
+//             <div class="todo-item ${t.status ? "active-todo" : ""}">
+//                 <div class="todo-item-title">
+//                     <input
+//                         type="checkbox"
+//                         ${t.status ? "checked" : ""}
+//                         onclick="toggleStatus(${t.id})"
+//                     />
+//                     <p>${t.title}</p>
+//                 </div>
+//                 <div class="option">
+//                     <button class="btn btn-update" onclick="updateTitle(${
+//                       t.id
+//                     })">
+//                         <img src="./img/pencil.svg" alt="icon" />
+//                     </button>
+//                     <button class="btn btn-delete" onclick=deleteTodo(${t.id})>
+//                         <img src="./img/remove.svg" alt="icon" />
+//                     </button>
+//                 </div>
+//             </div>
+//         `;
+//   }
+// }
+async function getTodos() {
+  const response = await getTodoAPI();
   todos = response.data;
-  renderUI(todos);
-  console.log(todos);
+  console.log(todos)
+  // renderUI(todos);
 }
 
-function createId() {
-  return Math.floor(Math.random() * 1000);
-}
+// function createId() {
+//   return Math.floor(Math.random() * 1000);
+// }
 
-async function createTodo(title) {
-  const response = await createTodoApi(title);
-  todos.push(response.data);
-  renderUI(todos);
-}
+// async function createTodo(title) {
+//   const response = await createTodoApi(title);
+//   todos.push(response.data);
+//   renderUI(todos);
+// }
 
-btnAdd.addEventListener("click", () => {
-  let todoTitle = todoInputEl.value;
-  if (todoTitle == "") {
-    alert("Không được để trống");
-    return;
-  }
-  if (isUpdate) {
-    let todo = todos.find((todo) => todo.id == idUpdate);
-    todo.title = todoTitle;
+// btnAdd.addEventListener("click", () => {
+//   let todoTitle = todoInputEl.value;
+//   if (todoTitle == "") {
+//     alert("Không được để trống");
+//     return;
+//   }
+//   if (isUpdate) {
+//     let todo = todos.find((todo) => todo.id == idUpdate);
+//     todo.title = todoTitle;
 
-    updateTodo(todo);
-  } else {
-    createTodo(todoTitle);
-  }
-  todoInputEl.value = "";
-});
+//     updateTodo(todo);
+//   } else {
+//     createTodo(todoTitle);
+//   }
+//   todoInputEl.value = "";
+// });
 
-async function deleteTodo(id) {
-  await deleteTodoApi(id);
+// async function deleteTodo(id) {
+//   await deleteTodoApi(id);
 
-  todos.filter((todo) => todo.id == id).splice(1);
-  renderUI(todos);
-}
+//   todos.filter((todo) => todo.id == id).splice(1);
+//   renderUI(todos);
+// }
 
-async function toggleStatus(id) {
-  let todo = todos.find((todo) => todo.id == id);
-  todo.status = !todo.status;
+// async function toggleStatus(id) {
+//   let todo = todos.find((todo) => todo.id == id);
+//   todo.status = !todo.status;
 
-  let res = await updateTodoApi(todo);
+//   let res = await updateTodoApi(todo);
 
-  todos.forEach((todo, index) => {
-    if (todo.id == id) {
-      todos[index] = res.data;
-    }
-  });
-  renderUI(todos);
-}
-function getOptionSelected() {
-  for (let i = 0; i < todoOptionEls.length; i++) {
-    if (todoOptionEls[i].checked) {
-      return todoOptionEls[i].value;
-    }
-  }
-}
+//   todos.forEach((todo, index) => {
+//     if (todo.id == id) {
+//       todos[index] = res.data;
+//     }
+//   });
+//   renderUI(todos);
+// }
+// function getOptionSelected() {
+//   for (let i = 0; i < todoOptionEls.length; i++) {
+//     if (todoOptionEls[i].checked) {
+//       return todoOptionEls[i].value;
+//     }
+//   }
+// }
 
-todoOptionEls.forEach((btn) => {
-  btn.addEventListener("change", function () {
-    let optionSelected = getOptionSelected();
-    getTodos(optionSelected);
-  });
-});
+// todoOptionEls.forEach((btn) => {
+//   btn.addEventListener("change", function () {
+//     let optionSelected = getOptionSelected();
+//     getTodos(optionSelected);
+//   });
+// });
 
-function updateTitle(id) {
-  let title;
-  todos.forEach((todo) => {
-    if (todo.id == id) {
-      title = todo.title;
-    }
-  });
+// function updateTitle(id) {
+//   let title;
+//   todos.forEach((todo) => {
+//     if (todo.id == id) {
+//       title = todo.title;
+//     }
+//   });
 
-  btnAdd.innerText = "CẬP NHẬT";
+//   btnAdd.innerText = "CẬP NHẬT";
 
-  todoInputEl.value = title;
-  todoInputEl.focus();
+//   todoInputEl.value = title;
+//   todoInputEl.focus();
 
-  idUpdate = id;
-  isUpdate = true;
-}
+//   idUpdate = id;
+//   isUpdate = true;
+// }
 
-async function updateTodo(todoUpdate) {
-  let response = await updateTodoApi(todoUpdate);
+// async function updateTodo(todoUpdate) {
+//   let response = await updateTodoApi(todoUpdate);
 
-  todos.forEach((todo, index) => {
-    if (todo.id == todoUpdate.id) {
-      todos[index] = response.data;
-    }
-  });
+//   todos.forEach((todo, index) => {
+//     if (todo.id == todoUpdate.id) {
+//       todos[index] = response.data;
+//     }
+//   });
 
-  btnAdd.innerText = "Thêm";
-  isUpdate = false;
-  idUpdate = null;
+//   btnAdd.innerText = "Thêm";
+//   isUpdate = false;
+//   idUpdate = null;
 
-  renderUI(todos);
-}
+//   renderUI(todos);
+// }
 
 window.onload = () => {
   getTodos();
 };
 //==================pagination==============//
-
-function loadItem() {
-  let beginGet = limit * (thisPage - 1);
-  let endGet = limit * thisPage - 1;
-  list.forEach((item, key) => {
-    if (key >= beginGet && key <= endGet) {
-      item.style.display = "block";
-    } else {
-      item.style.display = "none";
-    }
-  });
-}
-loadItem();
-
